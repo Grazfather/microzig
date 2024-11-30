bc49021a55afcc7c7734a1485fcf278ea5a408b7
Grazfather <grazfather@gmail.com>
2024-11-30 10:17:11 -0500

    rp2xxx: ws2812: Use DMA

--- examples/raspberrypi/rp2xxx/src/rp2040_only/ws2812.zig
+++ examples/raspberrypi/rp2xxx/src/rp2040_only/ws2812.zig
@@ -3,70 +3,50 @@ const microzig = @import("microzig");
 const rp2xxx = microzig.hal;
 const gpio = rp2xxx.gpio;
 const Pio = rp2xxx.pio.Pio;
+const Irq = rp2xxx.pio.Irq;
 const StateMachine = rp2xxx.pio.StateMachine;
 const dma = rp2xxx.dma;
+
+const pin_config = rp2xxx.pins.GlobalConfiguration{
+    .GPIO25 = .{
+        .name = "led",
+        .direction = .out,
+    },
+};
 
 const ws2812_program = blk: {
     @setEvalBranchQuota(10000);
     const p = rp2xxx.pio.assemble(
         \\.program ws2812
-        \\
-        \\.define public T1 2
-        \\.define public T2 5
-        \\.define public T3 3
-        \\
-        // \\ ; ORIGINAL
-        // \\.wrap_target
-        // \\bitloop:
-        // \\    out x, 1       side 0 [T3 - 1] ; Side-set still takes place when instruction stalls
-        // \\    jmp !x do_zero side 1 [T1 - 1] ; Branch on the bit we shifted out. Positive pulse
-        // \\do_one:
-        // \\    jmp  bitloop   side 1 [T2 - 1] ; Continue driving high, for a long pulse
-        // \\do_zero:
-        // \\    nop            side 0 [T2 - 1] ; Or drive low, for a short pulse
-        // \\.wrap
-        // \\
-        // \\ ; FANCY ONE
-        // \\.side_set 1 opt
-        // // \\    pull block                  ; pull the next data (number of LEDs - 1)
-        // \\    out y, 32                   ; and put it to "Y"
-        // // \\
-        // \\.wrap_target
-        // \\    mov x, y                    ; initialise the LED counter "X" with the value of "Y"
-        // \\next_led:
-        // // \\    pull block                  ; pull the next data (RGB value for a LED), blocking
-        // \\    irq 0                       ; set INT 0 flag
-        // // \\    out null, 8                 ; ignore the trailing 8 bits
-        // \\
-        // \\next_bit:
-        // \\    out x, 1 side 0 [T3 - 1]
-        // \\    nop side 1    [T1 - 1]
-        // \\    mov pins, x     [T2 - 2]
-        // \\    jmp next_bit
-        // \\    jmp x--, next_led
-        // \\    nop side 1      [T1 - 1]   ; set the GPIO to "1"
-        // \\    out pins, 1     [T2 - 1]    ; the middle 1/3 is equal to actual value we send
-        // \\    jmp !osre next_bit side 0 [T3 -1]  ; set the GPIO to "0", jump if the shift register contains more bits
-        // \\                                ; shift register empty -> no more data to send for a LED
-        // \\
-        // \\    jmp x-- next_led            ; jump if more LEDs to send data to
-        // \\                                ; here we are done - introduce the 50us reset delay
-        // \\                                ; wait 400 cycles (50us = 40 bit lengths * 3)
-        // \\    set x, 24                   ; 400 = 25 * 16 (set "X" to 24-1)
-        // \\delay_loop:
-        // \\    nop                [7]
-        // \\    jmp x-- delay_loop [7]      ; 1 + 7 + 1 + 7 cycles in each iteration
-        // \\
-        //        \\    irq clear 0                 ; clear INT 0 flag
-        //        simple working in python
         \\.side_set 1 opt
+        \\
+        \\    pull block                  ; pull the next data (number of LEDs - 1)
+        \\    out y, 32                   ; and put it to "Y"
+        \\
         \\.wrap_target
-        \\bitloop:
-        \\out x, 1 side 0 [T3 - 1]
-        \\jmp !x do_zero side 1 [T1 - 1]
-        \\jmp bitloop side 1 [T2 - 1]
-        \\do_zero:
-        \\nop side 0 [T2 - 1]
+        \\    mov x, y                    ; initialise the LED counter "X" with the value of "Y"
+        \\    set pins, 0                 ; Turn off status LED
+        \\next_led:
+        \\    pull block                  ; pull the next data (RGB value for a LED)
+        \\    irq 0                       ; set INT 0 flag
+        \\    out null, 8                 ; ignore the trailing 8 bits
+        \\
+        \\next_bit:
+        \\    set pins, 1 side 1          ; Set status LED and output led to 1
+        \\
+        \\    out pins, 1                 ; the middle 1/3 is equal to actual value we send
+        \\    jmp !osre, next_bit side 0  ; set the GPIO to "0", jump if the shift register contains more bits
+        \\                                ; shift register empty -> no more data to send for a LED
+        \\
+        \\    jmp x--, next_led           ; jump if more LEDs to send data to
+        \\                                ; here we are done - introduce the 50us reset delay
+        \\                                ; wait 120 cycles (50us = 40 bit lengths * 3)
+        \\    set x, 14                   ; 120 = 15 * 8 (set "X" to 15-1)
+        \\delay_loop:
+        \\    set pins, 0
+        \\    jmp x-- delay_loop [6]      ; 1 + 7 cycles in each iteration
+        \\
+        \\    irq clear 0                 ; clear INT 0 flag
         \\.wrap
     , .{}).get_program_by_name("ws2812");
     break :blk p;
@@ -74,109 +54,123 @@ const ws2812_program = blk: {
 
 const pio: Pio = .pio0;
 const sm: StateMachine = .sm0;
-const led_pin = gpio.num(22);
+const ws2812_pin = gpio.num(16);
+const led_pin = gpio.num(14);
 
 pub fn main() void {
+    pio.gpio_init(ws2812_pin);
     pio.gpio_init(led_pin);
-    // TODO: For the fancier one, it expects outshift false false 32
-    // https://www.youtube.com/watch?v=OenPIsmKeDI
+    // const pins = pin_config.apply();
+    // led_pin is used as set pin
     sm_set_consecutive_pindirs(pio, sm, @intFromEnum(led_pin), 1, true);
+    // ws2812 is used as out pin AND sideset fin
+    sm_set_consecutive_pindirs(pio, sm, @intFromEnum(ws2812_pin), 1, true);
 
-    // 52.08333206176758. Same as the C one
-    // const div = (@as(f32, @floatFromInt(rp2040.clock_config.sys.?.output_freq)) / 1e6) * 1.25 / 3.0;
-    // 10
-    const cycles_per_bit: comptime_int = ws2812_program.defines[0].value + //T1
-        ws2812_program.defines[1].value + //T2
-        ws2812_program.defines[2].value; //T3
-    // 15.625
-    const div = @as(f32, @floatFromInt(rp2xxx.clock_config.sys.?.frequency())) /
-        (800_000 * cycles_per_bit);
+    // Enable the interrupt that will be set in the PIO program
+    const irq = Irq.irq0;
+    pio.sm_enable_interrupt(sm, irq, Irq.Source.statemachine);
 
-    // @compileLog(ws2812_program.side_set);
+    const div = @as(f32, @floatFromInt(rp2xxx.clock_config.sys.?.frequency())) / 1e6 * 1.25 / 3;
+
     // This loads the PIO program, setting the side set, wrap, etc. based on the Program returned from the assembler
     pio.sm_load_and_start_program(sm, ws2812_program, .{
         .clkdiv = rp2xxx.pio.ClkDivOptions.from_float(div),
         .pin_mappings = .{
+            // .in doesn't take a count, so it is JUST a start index
+            // .in = .{}
+            .out = .{
+                .base = @intFromEnum(ws2812_pin),
+                .count = 1,
+            },
             // This needs to be set if we use set or mov
             .set = .{
                 .base = @intFromEnum(led_pin),
-                .count = 1,
+                .count = 2,
             },
-            // This needs to be set if we use side set
             .side_set = .{
-                .base = @intFromEnum(led_pin),
-                // We use 2, 1 side plus 1 for opt
+                .base = @intFromEnum(ws2812_pin),
+                // Need an extra if we make sideset optional
                 .count = 2,
                 // .count = 1,
             },
         },
         .shift = .{
-            // We use left for fancy as well
             .out_shiftdir = .left,
-            .autopull = true,
+            // .autopull = true,
             // 0 means 32
-            .pull_threshold = 0,
-            // .pull_threshold = 24,
-            // .join_tx = true,
+            // .pull_threshold = 0,
+            .join_tx = true,
         },
     }) catch unreachable;
 
     pio.sm_set_enabled(sm, true);
 
-    const LED_COUNT = 8;
-    // const colours = [_]u32{ 0xff0000, 0x00ff00, 0x0000ff };
-    const colours = [_]u32{ 0x00ACCC, 0x1D5DC5, 0x541ECB, 0x9C1CEE, 0xB30D8B, 0xBC1303, 0xD6AB01, 0xBEFE00, 0x69FF03, 0x26FF00, 0x00FF19 };
+    const LED_COUNT = 1;
+    // const colours = [_]u32{ 0x440000, 0x004400, 0x000044, 0x224488 };
+    const colours = [_]u32{ 0x008CAC, 0x0D3DA5, 0x340EAB, 0x7C0CDE, 0x930D6B, 0x0C0303, 0xB68B01, 0x9EDE00, 0x49DF03, 0x06DF00, 0x00DF09, 0x000000 };
+    // const colours = [_]u32{ 0x00ACCC, 0x1D5DC5, 0x541ECB, 0x9C1CEE, 0xB30D8B, 0xBC1303, 0xD6AB01, 0xBEFE00, 0x69FF03, 0x26FF00, 0x00FF19, 0x000000 };
     var led_buffer = [_]u32{0} ** LED_COUNT;
     var first_colour: u32 = 0;
 
     // TODO: Claim the channel?
     // TODO: Get default config?
     // Config DMA to copy LED buffer to the PIO statemachine's FIFO
-    // dma_channel_config dma_ch0 = dma_channel_get_default_config(led_dma_chan);
-    // const DREQ_PIO0_TX0 = 0;
-    // // var dma_ch0 = dma.channel(0);
-    // var dma_ch = dma.claim_unused_channel() orelse unreachable;
-    // var dma_config = dma.Channel.TransferConfig{
-    //     .transfer_size_bytes = 2,
-    //     .enable = true,
-    //     .read_increment = true,
-    //     .write_increment = false,
-    //     .dreq = @enumFromInt(DREQ_PIO0_TX0),
-    // };
-    // var addr = pio.sm_get_tx_fifo(sm);
-    // _ = addr;
-    // _ = dma_ch;
-    // _ = dma_config;
-    // channel_config_set_transfer_data_size(&dma_ch, DMA_SIZE_32); // 32 bits == 4 bytes, but is that allowed?
-    // channel_config_set_read_increment(&dma_ch, true);
-    // channel_config_set_write_increment(&dma_ch, false);
-    // channel_config_set_dreq(&dma_ch, DREQ_PIO0_TX0);
+    const DREQ_PIO0_TX0 = 0;
+    // This returns null for me!
+    // var dma_ch = dma.claim_unused_channel();
+    var dma_ch = dma.channel(0);
+    const dma_config = dma.Channel.TransferConfig{
+        // Transfer 32 bits each time. The program ignores the first 8, and 24
+        // bits isn't supported by the DMA
+        .transfer_size_bytes = 4,
+        .enable = true,
+        // Increment the address we read from after each write
+        .read_increment = true,
+        // But DON'T increment the write address: We are writing to a register
+        .write_increment = false,
+        // Use the PIO0 TX0 FIFO to request data
+        .dreq = @enumFromInt(DREQ_PIO0_TX0),
+    };
+    const addr = pio.sm_get_tx_fifo(sm);
 
-    // Tell PIO how many LEDs we have
+    const uart_tx_pin = gpio.num(0);
+    const uart_rx_pin = gpio.num(1);
+    inline for (&.{ uart_tx_pin, uart_rx_pin }) |pin| {
+        pin.set_function(.uart);
+    }
+    const uart = rp2xxx.uart.instance.num(0);
+    uart.apply(.{
+        .baud_rate = 115200,
+        .clock_config = rp2xxx.clock_config,
+    });
+    rp2xxx.uart.init_logger(uart);
+
     pio.sm_blocking_write(sm, LED_COUNT - 1);
 
     while (true) {
         // Rotate colours
         for (0..LED_COUNT) |i| {
             led_buffer[i] = colours[(i + first_colour) % colours.len];
-            pio.sm_blocking_write(sm, led_buffer[i] << 8);
         }
-        // dma_channel_configure(led_dma_chan, &dma_ch, &pio->txf[sm], led_buffer, LED_COUNT, true);
-        // dma_ch.trigger_transfer(@intFromPtr(addr), @intFromPtr(&led_buffer), LED_COUNT, dma_config);
-        rp2xxx.time.sleep_ms(50);
-
         first_colour = (first_colour + 1) % colours.len;
 
-        // rp2040.time.sleep_ms(500);
-        // pio.sm_blocking_write(sm, led_buffer[0] << 8);
-        // pio.sm_blocking_write(sm, 0x00ff00 << 8); //red
-        // pio.sm_blocking_write(sm, 0xff0000 << 8); //green
-        // rp2040.time.sleep_ms(50);
-        // pio.sm_blocking_write(sm, 0x0000ff << 8); //blue
-        // rp2040.time.sleep_ms(50);
-        // pio.sm_blocking_write(sm, 0x00ff00 << 8); //blue
+        // Wait for interrupt from PIO indicating it's done
+        while (get_irq_regs(irq).*.status.raw & (1 << 8) != 0) {}
+        // TODO: Had to write this function and hardcode pio0
+        // while (*get_irq_regs(pio, irq).status) {}
+        dma_ch.trigger_transfer(@intFromPtr(addr), @intFromPtr(&led_buffer), LED_COUNT, dma_config);
+
         rp2xxx.time.sleep_ms(250);
     }
+}
+
+fn get_irq_regs(irq: Irq) *volatile Irq.Regs {
+    // fn get_irq_regs(self: Pio, irq: Irq) *volatile Irq.Regs {
+    const PIO0 = microzig.chip.peripherals.PIO0;
+    const pio_regs = PIO0;
+    // const pio_regs = self.get_regs();
+    const irq_regs = @as(*volatile [2]Irq.Regs, @ptrCast(&pio_regs.IRQ0_INTE));
+    return &irq_regs[@intFromEnum(irq)];
 }
 
 fn sm_set_consecutive_pindirs(_pio: Pio, _sm: StateMachine, pin: u5, count: u3, is_out: bool) void {
@@ -186,6 +180,7 @@ fn sm_set_consecutive_pindirs(_pio: Pio, _sm: StateMachine, pin: u5, count: u3, 
         .SET_BASE = pin,
         .SET_COUNT = count,
     });
+    // Execute a `set pindirs, <is_out>` instruction
     _pio.sm_exec(_sm, rp2xxx.pio.Instruction{
         .tag = .set,
         .delay_side_set = 0,
